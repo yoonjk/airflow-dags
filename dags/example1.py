6cdd21e086494439d0359e0af7d1b687c63f5b42
yoonjk <nexweb1@gmail.com>
2022-10-12 21:46:23 +0900

    Remove fail task

--- dags/example1.py
+++ dags/example1.py
@@ -31,21 +31,9 @@ passing = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
 
 end = DummyOperator(task_id='end', dag=dag)
 
 
 passing.set_upstream(start)
-failing.set_upstream(start)
 passing.set_downstream(end)
-failing.set_downstream(end)
