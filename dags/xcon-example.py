8f9eaff7822171606874e8dc4781805206198a39
yoonjk <nexweb1@gmail.com>
2022-10-13 18:37:56 +0900

    Add xcon example

---
+++ dags/xcon-example.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+default_args = {
+    "owner": "MLOps",
+    "description": "An example",
+    "start_date": datetime(2022, 10, 13),
+}
+@dag(
+    "example",
+    default_args=default_args,
+    catchup=False,
+    schedule_interval=None,
+)
+def example_dag:
+    @task()
+    def hello_world() -> str:
+        return "hello, world!"
+
+    @task()
+    def display(msg: str):
+        print(msg)
+
+    # XComArgs를 통해 함수 호출 Style로 테스크 제어.
+    msg = hello_world()
+    display(msg)
+
+example_dag = example_dag()
