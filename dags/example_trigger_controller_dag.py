3aed2b13f747f0f0861ffd5d8d18ec308b88efc3
yoonjk <nexweb1@gmail.com>
2022-10-13 19:26:33 +0900

    trigger another tag

---
+++ dags/example_trigger_controller_dag.py
@@ -0,0 +1,20 @@
+"""
+from __future__ import annotations
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+with DAG(
+    dag_id="example_trigger_controller_dag",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    schedule="@once",
+    tags=['example'],
+) as dag:
+    trigger = TriggerDagRunOperator(
+        task_id="test_trigger_dagrun",
+        trigger_dag_id="example_trigger_target_dag",  # Ensure this equals the dag_id of the DAG to trigger
+        conf={"message": "Hello World"},
+    )
