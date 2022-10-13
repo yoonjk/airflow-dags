f7d75beacca65c0ce4fe34a305d6b5cd37b98875
yoonjk <nexweb1@gmail.com>
2022-10-13 19:09:49 +0900

    Fixed bug

--- dags/xcon-example.py
+++ dags/xcon-example.py
@@ -1,28 +1,50 @@
-from datetime import datetime
+import json
+from typing import Dict
+
+import pendulum
 from airflow.decorators import dag, task
-default_args = {
-    "owner": "MLOps",
-    "description": "An example",
-    "start_date": datetime(2022, 10, 13),
-}
+
+
+# 1. Define a dag using the @dag decorator
 @dag(
-    "example",
-    default_args=default_args,
-    catchup=False,
     schedule_interval=None,
+    start_date=pendulum.datetime(2022, 10, 13, tz="UTC"),
+    tags=["example"],
 )
+def example_taskflow_api():
 
-def example_dag():
+    # 2. Define tasks using the @task decorator
     @task()
-    def hello_world() -> str:
-        return "hello, world!"
+    def extract() -> Dict[str, int]:
+        data_string = '{"land1": 80, "land2": 75, "land3": 19}'
+
+        land_data_dict = json.loads(data_string)
+
+        return land_data_dict
 
     @task()
-    def display(msg: str):
-        print(msg)
+    def transform(land_data_dict: Dict[str, int]) -> Dict[str, int]:
+        total_value = 0
+        multi_value = 1
+        for value in land_data_dict.values():
+            total_value += value
+            multi_value *= value
 
-    # XComArgs를 통해 함수 호출 Style로 테스크 제어.
-    msg = hello_world()
-    display(msg)
+        return {"total_value": total_value, "multi_value": multi_value}
 
-dag=example_dag()
+    @task()
+    def load_total(total_value: int) -> None:
+        print("Total value is: %d" % total_value)
+
+    @task()
+    def load_multiple(multiple_value: int) -> None:
+        print("Multiple value is: %d" % multiple_value)
+
+    # 3. Define data (task) dependencies
+    land_data = extract()
+    order_summary = transform(land_data)
+    load_total(order_summary["total_value"])
+    load_multiple(order_summary["multi_value"])
+
+
+dag = example_taskflow_api()
