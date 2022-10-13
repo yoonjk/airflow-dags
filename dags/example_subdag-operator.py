1e644c6bdad4c2886f041e6e77a38a8422ba3c1f
yoonjk <nexweb1@gmail.com>
2022-10-13 19:54:23 +0900

    Add subdag example

---
+++ dags/example_subdag-operator.py
@@ -0,0 +1,44 @@
+"""Example DAG demonstrating the usage of the SubDagOperator."""
+from __future__ import annotations
+
+# [START example_subdag_operator]
+import datetime
+
+from airflow import DAG
+from airflow.dags.subdags.subdag import subdag
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.subdag import SubDagOperator
+
+DAG_NAME = 'example_subdag_operator'
+
+with DAG(
+    dag_id=DAG_NAME,
+    default_args={"retries": 2},
+    start_date=datetime.datetime(2022, 1, 1),
+    schedule="@once",
+    tags=['example'],
+) as dag:
+
+    start = EmptyOperator(
+        task_id='start',
+    )
+
+    section_1 = SubDagOperator(
+        task_id='section-1',
+        subdag=subdag(DAG_NAME, 'section-1', dag.default_args),
+    )
+
+    some_other_task = EmptyOperator(
+        task_id='some-other-task',
+    )
+
+    section_2 = SubDagOperator(
+        task_id='section-2',
+        subdag=subdag(DAG_NAME, 'section-2', dag.default_args),
+    )
+
+    end = EmptyOperator(
+        task_id='end',
+    )
+
+    start >> section_1 >> some_other_task >> section_2 >> 
---
+++ dags/subdags/subdag.py
@@ -0,0 +1,37 @@
+"""Helper function to generate a DAG and operators given some arguments."""
+from __future__ import annotations
+
+# [START subdag]
+import pendulum
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+def subdag(parent_dag_name, child_dag_name, args):
+    """
+    Generate a DAG to be used as a subdag.
+    :param str parent_dag_name: Id of the parent DAG
+    :param str child_dag_name: Id of the child DAG
+    :param dict args: Default arguments to provide to the subdag
+    :return: DAG to use as a subdag
+    :rtype: airflow.models.DAG
+    """
+    dag_subdag = DAG(
+        dag_id=f'{parent_dag_name}.{child_dag_name}',
+        default_args=args,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        schedule="@daily",
+    )
+
+    for i in range(5):
+        EmptyOperator(
+            task_id=f'{child_dag_name}-task-{i + 1}',
+            default_args=args,
+            dag=dag_subdag,
+        )
+
+    return dag_subdag
+
+
